b3ff9e4ef69e86b587ab9eea15d3577eef1e0162
Mikhail Alexeev <mishalxv35@gmail.com>
2023-01-13 10:13:04 -0800

    initial commit

---
+++ binds.py
@@ -0,0 +1,11 @@
+from kmk.keys import KC
+from kmk.handlers.sequences import simple_key_sequence
+
+ALL = KC.LCTL(KC.A)
+SAVE = KC.LCTL(KC.S)
+CUT = KC.LCTL(KC.X)
+COPY = KC.LCTL(KC.C)
+UNDO = KC.LCTL(KC.Z)
+PASTE = KC.LCTL(KC.V)
+
+
